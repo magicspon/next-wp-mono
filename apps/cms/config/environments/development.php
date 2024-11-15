9462a226074cddd58f9191868bf3a0d3cc58de3a
David Stockley <magicspon@gmail.com>
2024-11-15 21:00:02 +0000

    refactor: site url

--- apps/cms/config/environments/development.php
+++ apps/cms/config/environments/development.php
@@ -13,7 +13,7 @@ Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
 Config::define('DISALLOW_INDEXING', true);
-Config::define('WP_HOME', 'http://localhost:3000');
+Config::define('WP_HOME', env('HEADLESS_URL'));
 
 ini_set('display_errors', '0');
 
